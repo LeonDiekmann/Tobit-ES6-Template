8b58b71c8dc966d11a225ddaeb7f7f90b9cc28f6
Leon Diekmann <Leon_Diekmann@web.de>
2018-08-28 09:05:54 +0200

    changed class export to function export

--- src/components/registerForm/formButtonRegister.js
+++ src/components/registerForm/formButtonRegister.js
@@ -1,15 +1,15 @@
 export default function buttonRegister(formName,formAddress,formMail,formComment,btnAdd) {
-        chayns.intercom.sendMessageToPage({ 
-            text: "Name: " + formName.value + "\nAdresse: " + formAddress.value + 
-            "\nE-Mail: " + formMail.value + "\nKommentar: " + formComment.value
-        }).then(function(data){
-            formName.value = "";
-            formAddress.value = "";
-            formMail.value = "";
-            formComment.value = "";
-            btnAdd.disabled = true;
-            if(data.status == 200)
-               chayns.dialog.alert('','Vielen Dank, ihre Site wurde hinzugefügt.');
-        });
-    }
+    chayns.intercom.sendMessageToPage({ 
+        text: "Name: " + formName.value + "\nAdresse: " + formAddress.value + 
+        "\nE-Mail: " + formMail.value + "\nKommentar: " + formComment.value
+    }).then(function(data){
+        formName.value = "";
+        formAddress.value = "";
+        formMail.value = "";
+        formComment.value = "";
+        btnAdd.disabled = true;
+        if(data.status == 200)
+            chayns.dialog.alert('','Vielen Dank, ihre Site wurde hinzugefügt.');
+    });
+}
 
